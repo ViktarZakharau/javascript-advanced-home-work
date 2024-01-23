edde7adbf38802a04189174872307ea085ea2707
Захаров Виктор <obrubschik@yandex.com>
2024-01-23 22:40:54 +0700

    Домашнее задание 2

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,9 @@
 // @ts-check
+
+const generateRandomNumber = (dice) => {
+  const maxNumber = Number(dice.substring(1, dice.length));
+  const rundomNumber = Math.floor(Math.random() * maxNumber + 1);
+  return rundomNumber;
+};
+
+console.log(generateRandomNumber('d20'));

424e893688c750eda3376dd9f14f96d906bcbef5
Захаров Виктор <obrubschik@yandex.com>
2024-02-06 22:04:39 +0700

    Домашнее задание 6

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,30 @@
-// @ts-check
+class Carlass {
+  #carBrand;
+
+  #carModel;
+
+  #carMileage;
+
+  constructor(carBrand, carModel, carMileage) {
+    this.#carBrand = carBrand;
+    this.#carModel = carModel;
+    this.#carMileage = carMileage;
+  }
+
+  info() {
+    console.log(`Марка: ${this.#carBrand}. Модель: ${this.#carModel}. Пробег: ${this.#carMileage}`);
+  }
+
+  get carMileage() {
+    return this.#carMileage;
+  }
+
+  set carMileage(km) {
+    this.#carMileage += km; //или this.#_carMileage = km;
+  }
+}
+
+const car = new Carlass('Audi', 'A6', 0);
+car.carMileage = 100;
+car.carMileage = 300;
+car.info();
